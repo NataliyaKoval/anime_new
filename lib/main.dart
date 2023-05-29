b451ee24cee9a90b964ce9c6b850d038a2cadaeb
NataliiaKoval <hakunata@gmail.com>
2023-05-29 10:03:33 +0200

    Remove old code

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,19 @@
 import 'package:anime_new/data/entity/entities.dart';
 import 'package:anime_new/di/providers.dart';
 import 'package:anime_new/presentation/screens/home_screen/widget/home_screen.dart';
-import 'package:anime_new/providers/theme_provider.dart';
 import 'package:anime_new/themes/theme_data.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:provider/provider.dart';
 
-import 'database/app_preferences.dart';
-
-// void main() {
-//   runApp(const MyApp());
-// }
 void main() async {
   await Hive.initFlutter();
   Hive.registerAdapter<AnimeEntity>(AnimeEntityAdapter());
   Hive.registerAdapter<AnimeImagesEntity>(AnimeImagesEntityAdapter());
   Hive.registerAdapter<AnimeJpgEntity>(AnimeJpgEntityAdapter());
-  Box box = await Hive.openBox<AnimeEntity>('favoriteAnimes');
-  
-  runApp(MultiProvider(providers: [
-    ChangeNotifierProvider<ThemeProvider>(
-      create: (_) => ThemeProvider(),
-    ),
-  ], child: const MyApp()));
+  await Hive.openBox<AnimeEntity>('favoriteAnimes');
+
+  runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
@@ -32,8 +22,8 @@ class MyApp extends StatefulWidget {
   @override
   State<MyApp> createState() => _MyAppState();
 
-static _MyAppState of(BuildContext context) =>
-    context.findAncestorStateOfType<_MyAppState>()!;
+  static _MyAppState of(BuildContext context) =>
+      context.findAncestorStateOfType<_MyAppState>()!;
 }
 
 class _MyAppState extends State<MyApp> {
@@ -47,25 +37,17 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-   AppPreferences appPreferences = AppPreferences();
-
     return MultiProvider(
       providers: providers.providers,
       child: MaterialApp(
         title: 'Anime',
         theme: AppTheme.lightTheme,
         darkTheme: AppTheme.darkTheme,
-        themeMode: context
-            .watch<ThemeProvider>()
-            .themeMode,
         home: Scaffold(
           appBar: AppBar(),
           body: HomeScreen(),
         ),
-        // home: FutureBuilder(
-        //   future: appPreferences.syncThemeMode(context),
-        //   builder: (context, snapshot) => const AnimeScreen(),
-        ),
+      ),
     );
   }
 }
